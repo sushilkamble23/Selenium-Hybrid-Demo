d6473f2180bd8e65d5c90b27fdcb57a670f4d7b2
Sushil <sushil.learning23@gmail.com>
2026-03-17 14:21:01 +0530

    first commit

---
+++ src/test/java/com/tutorialsninja/qa/testcases/Login.java
@@ -0,0 +1,42 @@
+package com.tutorialsninja.qa.testcases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login {
+	WebDriver driver;
+	@BeforeMethod()
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(8));
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.linkText("Login")).click();
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void verifyLoginWithValidCredentials() {
+		driver.findElement(By.id("input-email")).sendKeys("sushil.kamble@test.com");
+		driver.findElement(By.id("input-password")).sendKeys("12345");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		
+	}
+
+}
