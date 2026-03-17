5fc487561c2b91ca6d0719d4e396567389f79112
Sushil <sushil.learning23@gmail.com>
2026-03-17 20:35:27 +0530

    added base package

---
+++ src/test/java/com/tutorialsninja/qa/base/Base.java
@@ -0,0 +1,33 @@
+package com.tutorialsninja.qa.base;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Base {
+	
+	WebDriver driver;
+	
+	public WebDriver initializeBrowserAndOpenApplicationURL(String browserName) {
+		
+		if (browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("safari")) {
+			driver = new SafariDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(8));
+		driver.get("https://tutorialsninja.com/demo/");
+		return driver;
+	}
+
+}
